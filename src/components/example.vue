3ddf5b449dfe2bca9089f1129c34e366171b1d7d
mouafa <mouafa.ahmed@smartfrog.com>
2017-10-23 23:00:01 +0200

    add example story with tests

---
+++ src/components/example.vue
@@ -0,0 +1,30 @@
+<style lang="stylus" scoped>
+@require '../stylus/_vars'
+
+.btn
+  background-color: $colors.orange.base
+
+</style>
+
+<template>
+    <button class="btn" dark fab fixed bottom right @click="clicked" >
+      <span>{{title}}</span>
+    </button>
+</template>
+
+<script>
+  export default {
+    name: 'kit-example',
+    props: {
+      title: {
+        type: String,
+        default: 'tada'
+      }
+    },
+    methods: {
+      clicked () {
+        this.$emit('click')
+      }
+    }
+  }
+</script>

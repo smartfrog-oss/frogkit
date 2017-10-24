2733a43a8c41795d095439a0fc6b0831c9722af8
mouafa <mouafa.ahmed@smartfrog.com>
2017-10-25 00:19:54 +0200

    add button slot test

--- __tests__/button.js
+++ __tests__/button.js
@@ -2,44 +2,46 @@ import { mount } from 'vue-test-utils'
 import Vue from 'vue'
 import Button from '@/components/button'
 
+const slots = { default: '<span>click me</span>' }
+
 describe('Button component', () => {
   it('should render component and match snapshot', () => {
-    const wrapper = mount(Button)
+    const wrapper = mount(Button, { slots })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render primary button', () => {
-    const wrapper = mount(Button, { propsData: { color: 'primary' } })
+    const wrapper = mount(Button, { slots, propsData: { color: 'primary' } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render secondary button', () => {
-    const wrapper = mount(Button, { propsData: { color: 'secondary' } })
+    const wrapper = mount(Button, { slots, propsData: { color: 'secondary' } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render small button', () => {
-    const wrapper = mount(Button, { propsData: { size: 'small' } })
+    const wrapper = mount(Button, { slots, propsData: { size: 'small' } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render big button', () => {
-    const wrapper = mount(Button, { propsData: { size: 'big' } })
+    const wrapper = mount(Button, { slots, propsData: { size: 'big' } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render outline button', () => {
-    const wrapper = mount(Button, { propsData: { outline: true } })
+    const wrapper = mount(Button, { slots, propsData: { outline: true } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render block button', () => {
-    const wrapper = mount(Button, { propsData: { block: true } })
+    const wrapper = mount(Button, { slots, propsData: { block: true } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 
   it('should render disabled button', () => {
-    const wrapper = mount(Button, { propsData: { disabled: true } })
+    const wrapper = mount(Button, { slots, propsData: { disabled: true } })
     expect(wrapper.html()).toMatchSnapshot()
   })
 

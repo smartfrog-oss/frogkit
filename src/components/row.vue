953efefa12f4d0f2bb1c5c2f20720da42642989a
mouafa <mouafa.ahmed@smartfrog.com>
2017-10-28 14:23:53 +0200

    flexbox layout example

--- src/components/row.vue
+++ src/components/row.vue
@@ -7,16 +7,13 @@
 </style>
 
 <template functional>
-  <div :class="`fk-row ${data.staticClass ? data.staticClass : ''} ${Object.keys(data.attrs)}`">
+  <div class="fk-row" :class="[data.staticClass, Object.keys(data.attrs)]">
     <slot></slot>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Row',
-    props: {
-      // size
-    }
+    name: 'Row'
   }
 </script>
---
+++ stories/flex.js
@@ -0,0 +1,39 @@
+import { storiesOf } from '@storybook/vue'
+import { action } from '@storybook/addon-actions'
+import { boolean, select } from '@storybook/addon-knobs'
+
+const stories = storiesOf('Flex Layout', module)
+
+stories.add('kitchen Sink', () => ({
+  template: `
+  <Grid style="height: 500px">
+    <Flex :column="column" :justify="justify" :align="align">
+      <div v-for="i in 8" :key="i" class="flex-demo" > {{i}} </div>
+    </Flex>
+  </Grid>
+  `,
+  data(){
+    return {
+      column: boolean('Column', false),
+      justify: select('Justify', ['start', 'center', 'end', 'space-around', 'space-between'], 'start'),
+      align: select('Align', ['start', 'center', 'end', 'stretch'], 'start')
+    }
+  }
+}))
+
+stories.add('Nested Flex', () => ({
+  template: `
+  <Grid style="height: 500px">
+    <Flex :column="column" :justify="justify" :align="align">
+      <Flex v-for="i in 8" :key="i" class="flex-demo" > {{i}} </Flex>
+    </Flex>
+  </Grid>
+  `,
+  data(){
+    return {
+      column: boolean('Column', false),
+      justify: select('Justify', ['start', 'center', 'end', 'space-around', 'space-between'], 'start'),
+      align: select('Align', ['start', 'center', 'end', 'stretch'], 'start')
+    }
+  }
+}))

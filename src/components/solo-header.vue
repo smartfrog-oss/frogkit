a9d61c59109a768ea53578641c8b1593cfe79836
Pierpaolo Ferraro <pierpaolo.ferraro@smartfrog.com>
2017-11-02 16:32:48 +0100

    add solo header component

---
+++ src/components/solo-header.vue
@@ -0,0 +1,29 @@
+<style lang="stylus" scoped>
+  @require '../stylus/mixins/solo-header'
+
+  .solo-header
+    soloheader-mixin()
+
+</style>
+
+<template functional>
+  <header class="solo-header">
+    <div class="solo-header__container">
+      <h1>
+        <a class="solo-header__container__logo" :href="props.logoUrl"><img src="../assets/logo.svg" /></a>
+      </h1>
+    </div>
+  </header>
+</template>
+
+<script>
+  export default {
+    name: 'Soloheader',
+    props: {
+      logoUrl: {
+          type: String,
+          default: '/'
+      }
+    }
+  }
+</script>

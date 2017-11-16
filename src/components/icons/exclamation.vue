f1f9af2a73a0aaa688aab0b958ec02ae0a07716e
mouafa <mouafa.ahmed@smartfrog.com>
2017-11-16 16:37:20 +0100

    add more icons

---
+++ src/components/icons/exclamation.vue
@@ -0,0 +1,1 @@
+<template functional><svg viewBox="0 0 15.6 62.5" v-bind="props" :class="[data.staticClass, data.class]"><path d="M10.2 37.1c1-3.4 5.3-18.3 5.3-27.8C15.6-3.1 0-3.1 0 9.4c0 9.4 4.1 23.9 5.3 27.8 1 3.2 4 3.2 4.9-.1z" class="st0"/><circle cx="7.7" cy="55.8" r="6.6" class="st0"/></svg></template>
---
+++ src/components/icons/facebook.vue
@@ -0,0 +1,1 @@
+<template functional><svg viewBox="0 0 40 40" v-bind="props" :class="[data.staticClass, data.class]"><path d="M32 0H8C3.6 0 0 3.6 0 8v24c0 4.4 3.6 8 8 8h12V24.5h-5.2v-6H20V14c0-5.2 3.2-8 7.8-8 2.2 0 4.1.2 4.7.2v5.4h-3.2c-2.5 0-3 1.2-3 2.9v3.9h6l-.8 6h-5.2V40H20h12c4.4 0 8-3.6 8-8V8c0-4.4-3.6-8-8-8z"/></svg></template>

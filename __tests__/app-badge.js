5605f997fb1a1ffd8c46d982799dd76cb12b034a
pierpaoloferraro <pierpolo.ferraro@smartfrog.com>
2018-01-17 11:25:26 +0100

    add fixes for tests

--- __tests__/app-badge.js
+++ __tests__/app-badge.js
@@ -2,24 +2,15 @@ import { mount } from 'vue-test-utils'
 import Vue from 'vue'
 import AppBadge from '@/components/app-badge'
 
-const appStoreItems = {
-  buttons: [
-    {
-      name: 'apple',
-      url: 'https://app.adjust.com/p39urb',
-      label: 'Laden im'
-    },
-    {
-      name: 'google',
-      url: 'https://app.adjust.com/vti0vj',
-      label: 'JETZT BEI'
-    }
-  ]
+const data = {
+  name: 'apple',
+  url: 'https://app.adjust.com/p39urb',
+  label: 'Laden im'
 }
 
 describe('App Badge component', () => {
   it('Should render component and match snapshot', () => {
-    const wrapper = mount(AppBadge, { propsData: appStoreItems })
+    const wrapper = mount(AppBadge, { propsData: data })
     expect(wrapper.html()).toMatchSnapshot()
   })
 })
--- __tests__/social-icon.js
+++ __tests__/social-icon.js
@@ -2,35 +2,15 @@ import { mount } from 'vue-test-utils'
 import Vue from 'vue'
 import SocialIcon from '@/components/social-icon'
 
-const socialItems = {
-  headline: 'Smartfrog folgen:',
-  icons: [
-    {
-      name: 'facebook',
-      url: 'http://www.facebook.com/smartfrogDE',
-      label: 'Folgen Sie auf Facebook'
-    },
-    {
-      name: 'twitter',
-      url: 'https://twitter.com/smartfrog_cam',
-      label: 'Auf Twitter folgen'
-    },
-    {
-      name: 'youtube',
-      url: 'https://www.youtube.com/channel/UCt2tBYCxnfhBWraIuOcY2Ow',
-      label: 'Folgen Sie auf YouTube'
-    },
-    {
-      name: 'instagram',
-      url: 'http://instagram.com/smartfrogcam/',
-      label: 'Folgen Sie auf Instagram'
-    }
-  ]
+const data = {
+  url: 'http://www.facebook.com/smartfrogDE',
+  name: 'facebook',
+  label: 'Folgen Sie auf Facebook'
 }
 
 describe('Social Icon component', () => {
   it('Should render component and match snapshot', () => {
-    const wrapper = mount(SocialIcon, { propsData: socialItems })
+    const wrapper = mount(SocialIcon, { propsData: data })
     expect(wrapper.html()).toMatchSnapshot()
   })
 })

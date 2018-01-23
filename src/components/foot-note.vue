16dca85bc6bc5655cc3c8a5fc99b5d80ed82a00e
Mohamed Ben Hadj Ltaief <mohamed.belhaj@smartfrog.com>
2018-01-23 15:45:49 +0100

    add note foot component

---
+++ src/components/foot-note.vue
@@ -0,0 +1,21 @@
+<style lang="stylus">
+  $text-color := #414141
+  $link-color := #2f82c4
+  .foot-note
+    font-size: 14px
+    color: $text-color
+    a
+      color: $link-color
+</style>
+
+<template>
+  <section class="foot-note">
+    <slot></slot>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'FootNote',
+  }
+</script>

f15ea65d67b7a77d9fa42e489798bab48b7cbf26
Mohamed Ben Hadj Ltaief <mohamed.belhaj@smartfrog.com>
2018-01-24 16:43:03 +0100

    add description block component

---
+++ src/components/description-block.vue
@@ -0,0 +1,25 @@
+<style lang="stylus">
+  @import '../stylus/mixins/description-block'
+  .fk-description-block
+    description-block-mixin()
+
+</style>
+
+<template>
+  <section class="fk-description-block">
+    <Title size="md" color="primary">{{title}}</Title>
+    <div class="fk-description-block__slot-wrapper">
+      <slot></slot>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'DescriptionBlock',
+    props: {
+      title: String,
+      default: ''
+    }
+  }
+</script>

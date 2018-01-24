181c121127416cc4c71a7832ce8e7679a5f398c9
Mohamed Ben Hadj Ltaief <mohamed.belhaj@smartfrog.com>
2018-01-24 15:06:33 +0100

    add test

--- stories/check-list.js
+++ stories/check-list.js
@@ -5,12 +5,12 @@ import { text, boolean, select } from '@storybook/addon-knobs'
 const stories = storiesOf('Check List', module)
 
 stories.addCodeExampleStory('Kitchen Sink', () => ({
-  template: `<CheckList :color="color" :bold="bold" :placeholder="defaultText">
+  template: `<CheckList :color="color" :bold="bold" :placeholder="placeHolder">
               <div>{{otherText}}</div>
               </CheckList>`,
   data () {
     return {
-      defaultText: text('Default Text', 'default text'),
+      placeHolder: text('Default Text', 'default text'),
       otherText: text('Other Text', 'other Text'),
       color: select('Color', ['orange', 'red', 'white'], 'orange'),
       bold: boolean('Bold', false)

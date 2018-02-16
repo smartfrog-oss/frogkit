420a588a5c6be71c7e7742e4d1c4e36b23cfb672
mouafa <mouafa.ahmed@smartfrog.com>
2018-02-16 20:05:30 +0100

    make icons normal components, input number fix

--- src/components/icons/angle-up.vue
+++ src/components/icons/angle-up.vue
@@ -1,1 +1,1 @@
-<template functional><svg preserveAspectRatio="xMidYMid" viewBox="0 0 11 5" v-bind="props" :class="[data.staticClass, data.class]"><path fill-rule="evenodd" d="M0 5.002h11L5.5-.001 0 5.002z"/></svg></template>
+<template><svg preserveAspectRatio="xMidYMid" viewBox="0 0 11 5"><path fill-rule="evenodd" d="M0 5.002h11L5.5-.001 0 5.002z"/></svg></template>

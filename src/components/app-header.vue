5e1d0f555d96a35b68a8a8a3cb67b7cc737b2821
Mohamed Ben Hadj Ltaief <mohamed.belhaj@smartfrog.com>
2018-04-18 09:40:37 +0200

    fix style

--- src/components/app-header.vue
+++ src/components/app-header.vue
@@ -7,76 +7,78 @@
 </style>
 
 <template>
-  <Grid>
-    <Col xs12>
-    <header class="fk-app-header">
-      <div class="fk-app-header__container">
-        <!-- logo -->
-        <h1 class="fk-app-header__logo" pid="header_button_home">
-          <a :href="logoUrl">
-            <Icon icon="logo-small" />
-          </a>
-        </h1>
-        <!-- navigation -->
-        <nav class="fk-app-header__nav">
-          <!-- all cameras -->
-          <div class="fk-app-header__cam" pid="header_button_all_cameras">
-            <a v-if="navigations[0]" :href="navigations[0].url">
-              <Icon icon="camera" /> {{ navigations[0].label }}</a>
-            <div class="fk-app-header__cam__nav">
-              <!-- <ul>
-              <li>
-                <a href="#" class="selected">Overview</a>
-              </li>
-              <li>
-                <a href="#">Saved videoclips</a>
-              </li>
-              <li>
-                <a href="#">Alerts and events</a>
-              </li>
-              <li>
-                <a href="#">Connect Smartfrog Cam</a>
-              </li>
-              <li>
-                <a href="#">Connect webcam</a>
-              </li>
-            </ul> -->
+  <Flex>
+    <Grid>
+      <Col xs12>
+      <header class="fk-app-header">
+        <div class="fk-app-header__container">
+          <!-- logo -->
+          <h1 class="fk-app-header__logo" pid="header_button_home">
+            <a :href="logoUrl">
+              <Icon icon="logo-small" />
+            </a>
+          </h1>
+          <!-- navigation -->
+          <nav class="fk-app-header__nav">
+            <!-- all cameras -->
+            <div class="fk-app-header__cam" pid="header_button_all_cameras">
+              <a v-if="navigations[0]" :href="navigations[0].url">
+                <Icon icon="camera" /> {{ navigations[0].label }}</a>
+              <div class="fk-app-header__cam__nav">
+                <!-- <ul>
+                <li>
+                  <a href="#" class="selected">Overview</a>
+                </li>
+                <li>
+                  <a href="#">Saved videoclips</a>
+                </li>
+                <li>
+                  <a href="#">Alerts and events</a>
+                </li>
+                <li>
+                  <a href="#">Connect Smartfrog Cam</a>
+                </li>
+                <li>
+                  <a href="#">Connect webcam</a>
+                </li>
+              </ul> -->
+              </div>
             </div>
-          </div>
-          <!-- my account -->
-          <a v-if="navigations[1]" :href="navigations[1].url" class="fk-app-header__account" pid="header_button_my_account">
-            <Icon icon="account" /> {{ navigations[1].label }}</a>
-          <!-- shop button -->
-          <Button v-if="navigations[2]" link color="secondary" size="small" class="fk-app-header__btn-shop" :href="navigations[2].url" pid="header_button_shop">
-            {{ navigations[2].label }}
-          </Button>
-          <!-- current user -->
-          <div class="fk-app-header__user">
-            <span>{{ username }}</span>
-          </div>
-          <!-- logout -->
-          <div v-if="logoutlabel" class="fk-app-header__logout" @click="logout()">
-            <Icon icon="angle-down" /> {{ logoutlabel }}
-          </div>
-          <!-- language selector -->
-          <div v-if="activeLang" class="fk-app-header__lang" :class="{ 'fk-app-header__lang--on': toggleLangList}" @click="toggleDropdown">
-            {{ activeLang }}
-            <Icon icon="angle-down" />
-            <ul v-if="toggleLangList" v-click-outside="closeDropDown">
-              <li v-for="lang in languages" :key="lang.code" @click="changeLang(lang.code)">
-                {{ lang.name }}
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <!-- burger menu icon -->
-        <!-- <span class="fk-app-header__toggle">
-        <Icon icon="hamburger-menu" />
-      </span> -->
-      </div>
-    </header>
-    </Col>
-  </Grid>
+            <!-- my account -->
+            <a v-if="navigations[1]" :href="navigations[1].url" class="fk-app-header__account" pid="header_button_my_account">
+              <Icon icon="account" /> {{ navigations[1].label }}</a>
+            <!-- shop button -->
+            <Button v-if="navigations[2]" link color="secondary" size="small" class="fk-app-header__btn-shop" :href="navigations[2].url" pid="header_button_shop">
+              {{ navigations[2].label }}
+            </Button>
+            <!-- current user -->
+            <div class="fk-app-header__user">
+              <span>{{ username }}</span>
+            </div>
+            <!-- logout -->
+            <div v-if="logoutlabel" class="fk-app-header__logout" @click="logout()">
+              <Icon icon="angle-down" /> {{ logoutlabel }}
+            </div>
+            <!-- language selector -->
+            <div v-if="activeLang" class="fk-app-header__lang" :class="{ 'fk-app-header__lang--on': toggleLangList}" @click="toggleDropdown">
+              {{ activeLang }}
+              <Icon icon="angle-down" />
+              <ul v-if="toggleLangList" v-click-outside="closeDropDown">
+                <li v-for="lang in languages" :key="lang.code" @click="changeLang(lang.code)">
+                  {{ lang.name }}
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <!-- burger menu icon -->
+          <!-- <span class="fk-app-header__toggle">
+          <Icon icon="hamburger-menu" />
+        </span> -->
+        </div>
+      </header>
+      </Col>
+    </Grid>
+  </Flex>
 </template>
 
 <script>
--- src/components/bundle-recap.vue
+++ src/components/bundle-recap.vue
@@ -1,37 +1,43 @@
 <style lang="stylus">
   @import '../stylus/_vars'
   .fk-bundle-recap
-    flex-wrap: nowrap !important
     background-color white
+    width 100%
     padding 10px
     & &__icon
       width 20px
       height 20px
-    &__item, &__item--small
-      width 25%
+    & &__item, &__item--small
+      width 80px
       position relative
       img
         max-height 100px
         display: block
         margin: 0 auto
     &__item--small
-      img
-        max-height 45px
-    & &__label
-      font-size: 1.5rem
+      @media(min-width $bp-sm)
+        width 25%
+        img 
+          max-width 45px
+    & &__label, &__label--small
+      font-size: 1.6rem
       text-align: center
+    &__label--small
+      font-size: 1.4rem
+    &__label-container
+      flex-wrap nowrap !important
 </style>
 
 <template>
   <section class="fk-bundle-recap">
-    <Flex align="center" :justify="justify" class="m-b-10">
+    <Flex align="center" justify="space-between" class="m-b-10">
       <div v-if="bundle[0]" :class="itemClass">
         <img :src="bundle[0].image" align="middle" />
       </div>
 
       <Icon v-if="bundle[1]" icon="plus-bold" color="orange" class="fk-bundle-recap__icon"/>
 
-      <div v-if="bundle[1]" class="fk-bundle-recap__item">
+      <div v-if="bundle[1]" :class="itemClass">
         <slot name="sticker"></slot>
         <img :src="bundle[1].image" align="middle" />
       </div>
@@ -43,9 +49,9 @@
       </div>
     </Flex>
 
-    <Flex align="center" :justify="labelJustify">
+    <Flex align="center" justify="space-between" class="fk-bundle-recap__label-container">
       <div v-for="(item, i) in bundle" :key="i" class="fk-bundle-recap__item">
-        <p class="fk-bundle-recap__label" v-html="item.label"></p>
+        <p :class="labelClass" v-html="item.label"></p>
       </div>
     </Flex>
   </section>
@@ -66,13 +72,10 @@
     },
     computed: {
       itemClass() {
-        return !!this.small ? 'fk-bundle-recap__item--small' : 'fk-bundle-recap__item' 
+        return !!this.small ? 'fk-bundle-recap__item--small' : 'fk-bundle-recap__item'
       },
-      justify() {
-        return !!this.small ? 'space-between' : 'space-evenly'
-      },
-      labelJustify() {
-        return !!this.small ? 'space-between' : 'space-around'
+      labelClass() {
+        return !!this.small ? 'fk-bundle-recap__label--small' : 'fk-bundle-recap__label'
       }
     }
   }

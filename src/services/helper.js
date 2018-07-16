a62068fc592e9c8427b3fb8a1b93a33412970952
Mohamed Ben Hadj Ltaief <mohamed.belhaj@smartfrog.com>
2018-07-16 11:54:36 +0200

    fix uk zip code validation

--- src/services/helper.js
+++ src/services/helper.js
@@ -13,12 +13,16 @@ export function formatUkZip (zip = '') {
       /* Format code to right format with 6 chars: XXXXXX => XXX XXX */
       return tmp.slice(0, 3).concat(' ').concat(tmp.slice(3))
     case 7:
+      if(tmp[2] === ' ') return tmp.slice(0, 2).concat(tmp.slice(3,4)).concat(' ').concat(tmp.slice(4))
       /* To prevent reformating zip code with  chars: XXX XXX  */
       if (tmp[3] === ' ') return tmp
       /* Format code to the right format: XXXX XX => XXX XXX */
       if(tmp[4] === ' ') return tmp.slice(0,3).concat(' ').concat(tmp.slice(3,4)).concat(tmp.slice(5))
       /* Format code to right format with 6 chars: XXXXXXX => XXXX XXX */
       return tmp.slice(0, 4).concat(' ').concat(tmp.slice(4))
+      case 8:
+      /* Format code to right format with 7 chars: XXX XXXX => XXXX XXX */
+        if (tmp[3] === ' ') return tmp.slice(0, 3).concat(tmp.slice(4,5)).concat(' ').concat(tmp.slice(5))
     default:
       return tmp
   }

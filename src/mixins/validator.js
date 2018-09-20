6ea9cd2e849f872c143f46fdf3320f9e54bd4b95
mouafa <mouafa.ahmed@smartfrog.com>
2018-09-20 11:32:33 +0200

    refactor: password strength

--- src/mixins/validator.js
+++ src/mixins/validator.js
@@ -1,21 +1,23 @@
+import password from '@smartfrog/password'
+
 const regex = {
   text: /.*/,
   houseNumber: /^[0-9a-zA-Z ]+$/,
   phone: /^[0-9 \\+\\-\\(\\)\\/]{3,20}$/,
-  password: /^[0-9a-zA-Z!*^?+-_@#$%&<>§±'":;.,/=~\[\]\{\}\`\(\)\|\\ ]{6,64}$/,
+  // password: /^[0-9a-zA-Z!*^?+-_@#$%&<>§±'":;.,/=~\[\]\{\}\`\(\)\|\\ ]{8,64}$/,
   email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
   // NOTE: uk zip code is provided by goverment
   // ukZip: /^(([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2}))$/
-  ukZip: /^(([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2}))$/
+  ukZip: /^(([Gg][Ii][Rr] 0[Aa]{2})|((([A-Za-z][0-9]{1,2})|(([A-Za-z][A-Ha-hJ-Yj-y][0-9]{1,2})|(([A-Za-z][0-9][A-Za-z])|([A-Za-z][A-Ha-hJ-Yj-y][0-9]?[A-Za-z]))))\s?[0-9][A-Za-z]{2}))$/,
 }
 
-export default function validator (model = 'value') {
+export default function validator(model = 'value') {
   return {
-    props:{
+    props: {
       wrong: {
         type: Boolean,
-        default: false
-      }
+        default: false,
+      },
     },
     data() {
       return {
@@ -25,8 +27,8 @@ export default function validator (model = 'value') {
         errors: {
           pattern: false,
           required: false,
-          lengthError: ''
-        }
+          lengthError: '',
+        },
       }
     },
     computed: {
@@ -34,14 +36,14 @@ export default function validator (model = 'value') {
         return Object.values(this.errors || {}).some(Boolean)
       },
       invalid() {
-        return this.wrong || this.errone && this.touched
-      }
+        return this.wrong || (this.errone && this.touched)
+      },
     },
     watch: {
       [model]() {
         this.touched = true
         this.runValidation()
-      }
+      },
     },
     mounted() {
       this.runValidation()
@@ -50,27 +52,34 @@ export default function validator (model = 'value') {
       runValidation() {
         this.errors = this.validate(this[model])
       },
-      validate(value, type='') {
+      validate(value, type = this.type, force = false) {
         const errors = {}
         if (this.disabled) return errors
-        if(this.match) {
+        if (this.match) {
           errors.pattern = value !== this.match
           return errors
         }
 
-        if (this.required || (this.requireValidation && value)) {
+        if (this.required || (this.requireValidation && value) || force) {
           errors.required = typeof value === 'string' ? !value.length : !value
-          errors.pattern = this.pattern && !this.pattern.test(value) || (typeof value === 'string' && !value.trim().length)
-          const inputType = type || this.type
-          if (inputType === 'password') {
-            if (value.length < 6) errors.lengthError = 'min'
-            else if (value.length > 64) errors.lengthError = 'max'
-            else  errors.lengthError = ''
+          errors.pattern =
+            (this.pattern && !this.pattern.test(value)) ||
+            (typeof value === 'string' && !value.trim().length)
+          if (type === 'password') {
+            Object.assign(errors, password.getErrors(value))
           }
         }
 
         return errors
-      }
-    }
+      },
+      getStatus(value) {
+        const passwordStatus = {
+          WEAK: 'danger',
+          MEDIUM: 'warning',
+          STRONG: 'success',
+        }
+        return passwordStatus[password.getStrength(value)]
+      },
+    },
   }
 }
--- stories/form.js
+++ stories/form.js
@@ -6,10 +6,10 @@ import Form from '@/components/form'
 
 const stories = storiesOf('Form', module)
 
-
-stories.addCodeExampleStory('Kitchen Sink', () => ({
-  template:
-  `<Form v-model="isValid" class="form-demo">
+stories.addCodeExampleStory(
+  'Kitchen Sink',
+  () => ({
+    template: `<Form v-model="isValid" class="form-demo">
     <div>
       <Select v-model="inputs[4]" :disabled="disabled" block placeholder="Title" :options="options" :wrong="wrong" required></Select>
     </div>
@@ -17,7 +17,14 @@ stories.addCodeExampleStory('Kitchen Sink', () => ({
       <Input v-model="inputs[0]" name="username" type="text" placeholder="username" :disabled="disabled" :wrong="wrong" required block></Input>
     </div>
     <div>
-      <Input v-model="inputs[1]" type="password" placeholder="password" :disabled="disabled" :wrong="wrong" required block></Input>
+      <InputTooltip :title="title" :conditions="conditions" :status-txt="statusTxt">
+        <InputPassword v-model="inputs[1]" placeholder="InputPassword" required/>
+      </InputTooltip>
+    </div>
+    <div>
+      <InputTooltip :title="title" :conditions="conditions" :status-txt="statusTxt">
+        <Input v-model="inputs[1]" type="password" placeholder="password" :disabled="disabled" :wrong="wrong" required block></Input>
+      </InputTooltip>
     </div>
     <div>
       <Input v-model="inputs[2]" type="phone" placeholder="phone" :disabled="disabled" :wrong="wrong" required block></Input>
@@ -28,27 +35,40 @@ stories.addCodeExampleStory('Kitchen Sink', () => ({
     <h1 class="demo-title">Valid: {{isValid}}</h1>
     <Button @click="submit" color="primary" block> Submit </Button>
   </Form>`,
-  data() {
-    return {
-      inputs: Array.from({length: 5}),
-      disabled: boolean('Disabled', false),
-      wrong: boolean('Wrong', false),
-      options: [{value: 'Mr', label: 'Mr'}, {value: 'Mrs', label: 'Mrs'}],
-      isValid: null
-    }
-  },
-  methods: {
-    submit() {
-      if (!this.isValid) return
-      alert('Form Submitted!')
-    }
-  }
-}), Form)
+    data() {
+      return {
+        inputs: Array.from({ length: 5 }),
+        disabled: boolean('Disabled', false),
+        wrong: boolean('Wrong', false),
+        options: [{ value: 'Mr', label: 'Mr' }, { value: 'Mrs', label: 'Mrs' }],
+        isValid: null,
+        conditions: {
+          length: '8 or more characters',
+          letters: 'Upper & lowercase letters',
+          digit: 'At least one number',
+        },
+        statusTxt: {
+          danger: 'weak password',
+          warning: 'good password',
+          success: 'strong password',
+        },
+        title: 'Password must have:',
+      }
+    },
+    methods: {
+      submit() {
+        if (!this.isValid) return
+        alert('Form Submitted!')
+      },
+    },
+  }),
+  Form
+)
 
-
-stories.addCodeExampleStory('Auto Scroll to Error', () => ({
-  template:
-  `<Form v-model="isValid" class="form-demo">
+stories.addCodeExampleStory(
+  'Auto Scroll to Error',
+  () => ({
+    template: `<Form v-model="isValid" class="form-demo">
     <div>
       <Input v-model="inputs[0]" name="username" type="text" placeholder="username" :disabled="disabled" required block></Input>
     </div>
@@ -71,17 +91,19 @@ stories.addCodeExampleStory('Auto Scroll to Error', () => ({
     <h1 class="demo-title">Valid: {{isValid}}</h1>
     <Button @click="submit" color="primary" block> Submit </Button>
   </Form>`,
-  data() {
-    return {
-      inputs: Array.from({length: 5}),
-      disabled: boolean('Disabled', false),
-      isValid: null
-    }
-  },
-  methods: {
-    submit() {
-      if (!this.isValid) return
-      alert('Form Submitted!')
-    }
-  }
-}), Form)
+    data() {
+      return {
+        inputs: Array.from({ length: 5 }),
+        disabled: boolean('Disabled', false),
+        isValid: null,
+      }
+    },
+    methods: {
+      submit() {
+        if (!this.isValid) return
+        alert('Form Submitted!')
+      },
+    },
+  }),
+  Form
+)

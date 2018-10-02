4a5a4d5c666dbb2bf92925b450070893b4fea1c5
pierpaoloferraro <pierpolo.ferraro@smartfrog.com>
2018-10-02 12:31:59 +0200

    style: add responsive behaviour

--- src/components/not-found.vue
+++ src/components/not-found.vue
@@ -1,27 +1,32 @@
 <style lang="stylus">
   .fk-not-found
     background-color #fff
-    height 500px
-    width 800px
+    min-height 500px
+    width 90%
+    max-width 800px
+    padding 40px 30px 60px
     border-radius 8px
     & &__exclamation
       width 95px
       height @width
+    & &__title
+      text-align center
     & &__content
       font-size 2.2rem
       color: #444
       text-align: center
     & &__button
-      width 300px
+      max-width 300px
+
 </style>
 
 <template>
   <Flex class="fk-not-found" align="center" justify="center" column>
     <Icon class="fk-not-found__exclamation m-b-30" icon="exclamation-circle" color="orange" />
-    <Title class="m-b-40" color="primary" size="xl">{{title}}</Title>
+    <Title class="fk-not-found__title m-b-40" color="primary" size="xl">{{title}}</Title>
     <p class="fk-not-found__content m-b-40" v-html="content">
     </p>
-    <Button class="fk-not-found__button" :href="link"  size="big" color="secondary" link outline>
+    <Button class="fk-not-found__button" :href="link"  size="big" color="secondary" link outline block>
       {{button}} <Icon icon="arrow-right" color="secondary"/>
     </Button>
   </Flex>

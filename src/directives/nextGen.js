012dfb4513a7d4278d58aea851a059dd247375b0
mohamed <mohamed.belhaj@smartfrog.com>
2019-02-05 15:26:39 +0100

    fix: tiny fix

--- src/directives/nextGen.js
+++ src/directives/nextGen.js
@@ -2,7 +2,6 @@ export default {
   inserted: async function(el, binding, vnode) {
     const webpSupported = binding.value
     const { src } = el
-    el.src = `${src}?fm=webp`
     if (src && src.includes('ctfassets') && !src.endsWith('svg') && webpSupported) el.src = `${src}?fm=webp`
   }
 }

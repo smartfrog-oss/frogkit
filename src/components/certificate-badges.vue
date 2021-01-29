3a925b0d8169c30cbd04166c761d75c41c5aac23
pierpaoloferraro <pierpaolo.ferraro@smartfrog.com>
2021-01-29 17:04:43 +0100

    remove av test logo

--- src/components/certificate-badges.vue
+++ src/components/certificate-badges.vue
@@ -3,7 +3,6 @@
     <img :width="width" src="../assets/images/iso-27001.jpg" alt="iso-27001" />
     <img :width="width" src="../assets/images/tuev.jpg" alt="tuev" />
     <img :width="width" src="../assets/images/iso-9001.jpg" alt="iso-9001" />
-    <img :width="width" src="../assets/images/av-test.svg" alt="av-test" />
   </Flex>
 </template>
 
